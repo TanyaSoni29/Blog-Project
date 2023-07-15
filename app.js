6b8f9ceb382c47b48cbdcfe2b20bd5042160f76b
tanusona <128149603+tanusona@users.noreply.github.com>
2023-07-16 01:07:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://tanya:Tanya790@cluster0.rxzdtbp.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema ={
   title: String,
